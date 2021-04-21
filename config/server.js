a4e1a6cab2a8e25b6ae43c7bef7e6dc3df74fc57
antoniojh10 <antoniohernandez95@gmail.com>
2021-04-21 15:33:18 -0400

    feat: :tada: first commit with strapi postgres local config

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ab557e2a7cb20393dedce740c3b29b22'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c0d384fa-dc85-4aab-b557-6deeb8d3b147'
+};
